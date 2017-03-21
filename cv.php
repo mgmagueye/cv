82fa8a178f80632b026936b36cdd6a43fd9d556e
mgmagueye <ndiaymacgy06@yahoo.fr>
2017-03-21 02:04:47 +0100

    Add files via upload

---
+++ cv.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+
+<html>
+
+	<head>
+		<title>magueye ndiaye</title>
+		 <link rel = "stylesheet" href="site_magueye.css"> 
+		 <meta http-equiv="refresh" content="10; URL="http://www.test.fr"/>
+		 <meta name="Publisher" content="Magueye NDIAYE"/>
+	</head>
+
+	
+	<body>
+	
+			<header>
+				 <div class="page">
+					<div id="magueye">
+					<h1><span class="highlight">Magueye </span>NDIAYE</h1>
+					</div>
+					<nav>
+						<ul>
+						<li class="current_1"><a href="accueil.php">Accueil</a></li>
+						<!--<li class="current_2"><a href="blog.php">Blog</a></li> -->
+						<li class="current_3"><a href="cv.php">CV</a></li>
+						<li class="current_4"><a href="photo.php">Photos</a></li>
+						<li class="current_5"> <a href="contact.php">Contact</a></li>
+						<li class="image_1"><a href="https://fr.linkedin.com/in/magueye-ndiaye-99334b108" target=_blank><img src="..\_photos\linkedin.png" > </a> </li>
+							<!--<li class="current_6"><a href="reseauxsociaux.php">Réseaux Sociaux</a></li> -->
+
+						</ul>
+					</nav>
+					
+				</div>
+			</header>
+			
+			
+			
+			
+	<!-- partie cv -->
+	
+	 
+	<!--<a href="https://mgmagueye.github.io/cv/"  target=_blank> Mon cv</a> -->
+	<section id="entetecv">
+	
+			<center><h1>Curriculum vitae</h1></center>
+	</section>
+	
+<!DOCTYPE html>
+<html>
+	<head>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<title> CV magueye NDIAYE </title>
+	<meta charset="utf-8"/>
+	<link rel = "stylesheet" href="site-cv-magueye.css">
+	<script type = "text/javaScript" src="site-cv.js"> </script>
+	
+	
+	</head>
+
+	<body>
+	<div class="centree1">
+		<a href="#competence_1">Competences Techniques</a> 	
+		<a href="#projet_1">Projets et Expériences Professionnelles</a>
+		<a href="#formation_1">Formation et Diplômes</a> 
+		<a href="#centre">Centre d'interêt</a>
+					
+	</div>
+	<div class="page">
+	<div class="infophoto">
+		<div class="info"> 
+		
+		<!--	<p><h2> <div class ="magueye" > <center>Magueye NDIAYE </center></div > </h2><br> -->
+					9 Rue du professeur CALMETTE <br>
+					69100 Villeurbanne <br>
+					Tel: <strong> 06 52 00 36 69 </strong> <br>
+					Age : 26 ans 
+					<div class="h2">
+					  mail: ndiaymacgy06@yahoo.fr 
+					 </div>
+				</p>
+		
+		</div>
+		<div class="photo"> 
+		
+					<img class="photo" src="..\_photos\image-cv.jpg" alt="photo magueye ndiaye" />
+		</div>
+	</div>	
+				
+				  
+				</tr>
+				
+			<div class = "formation">
+				<!-- <span style ="text-decoration: underline;"> -->
+					<h2>FORMATION ET DIPLOMES
+					<button style="font-size: 50%;height:20px;padding-left:20px;padding-right:20px;padding-bottom:20px;background:#e8491d;border :#cccccc 2px solid" onclick="afficher('formation_1');">cacher</button>
+					</h2>
+					<!-- </span> -->
+		</div>
+		<div id="formation_1">
+		<div class="formation_1">
+					<strong>2016-2017 : Master 2, Informatique, Spécialite Compétence en Informatique</strong> <br>
+					<strong>2015-2016 : Diplômé de l'école d'ingenieur Polytech Montpellier, Spécialite énergetique et énergies renouvelables </strong> Montpellier France <br>
+					<strong>2009-2010 : Baccalauréat Scientifique</strong> Dakar, Sénégal <br>
+		</div>
+		</div>
+
+
+
+
+			
+		<div class="competence">
+		
+			<!-- <span style ="text-decoration: underline;"> -->
+						<h2>COMPETENCES TECHNIQUES 
+						<button style="font-size: 50%;height:20px;padding-left:20px;padding-right:20px;padding-bottom:20px;background:#e8491d;border :#cccccc 2px solid" onclick="afficher('competence_1');">cacher</button>
+						</h2>	
+				<!-- </span>	 -->					
+		</div>
+		<div id="competence_1">
+			<div class="competence_1">
+		
+					<p> 
+					<li>Langages de programmation : <strong> C,Java, Java/J2EE, PHP,html5/CSS3,javaScript,shell,VBA </strong><br>
+					<li>Langages de Simulation : <strong>  Matlab/ Simulink</strong> <br>
+					<li>Base de données: <strong> SGBD, SQL server, MySQL, Oracle</strong> <br>
+					<li>Analyse et Conception : Merise, UML <br>
+					<li>Système d'exploitation : (Unix (MacOs X, GNU/Linux Debian, Ubuntu)),Windows<br>
+					<li>Français : Langue maternelle <br>
+					<li>Anglais : Niveau professionnel <br>
+					<li>Logiciels : Pack office, PVsyst, Perrenoud, Pleiades+COMFIE, Comsol, AutoCAD, Matlab.<br>
+					</li>
+					</p>
+			</div>	
+		</div>
+		<div class="projet">
+			<!-- <span style ="text-decoration: underline;"> -->
+					<h2>PROJETS ET EXPERIENCES PROFESSIONNELLES
+					<button style="font-size: 50%;height:20px;padding-left:20px;padding-right:20px;padding-bottom:20px;background:#e8491d;border :#cccccc 2px solid" onclick="afficher('projet_1');">cacher</button>
+					</h2>
+					<!-- </span> -->
+		</div>	
+		<div id="projet_1">
+		<div class="projet_1">
+					Mars 2016 - Aout 2016 : <strong>Stage de fin d'etudes : Bureau d'études thermiques et fluides</Strong>, EFFICEN, France <br>
+					<li> Projet 1 : Évaluation des consommations énergetiques par simulation thermique dynamique (Pleiades+COMFIE)</li>
+					<li>Projet 2 : Réalisation d'audits énergetiques 
+						<UL TYPE="circle">
+						<li>Diagnostic téchnique du bâtiment et des équipements, réalisation des plan, enquête auprès des occupants</li>
+						<li>Étude thermique, proposition des bouquets de travaux améliorant la qualite thermique, acoustique du bâtiment</li>
+						<li>Proposition des conseils de comportements individuels pour plus d'économie d'énergie</li>
+						<li>Réalisation des éventuelles études de faisabilite bois énergie, solaire thermique et photovoltaîque</li>
+						<li>Montage du dossier financier spécifique au projet (aide de l'ANAH, prime rénovation énergetique, crédit d'impôt développenment durable, certificat d'économie d'énergie, éco-pret au taux zero</li>
+						<li>Prénsentation des conclusions de l'audit auprès des syndics de coproprietes lors de l'assemblée generale</li>
+						</UL>
+					<li>Projet de fin d'études : Développement d'un outil d'audit énergetique (langage de programmation Visual basic d'excel)</li> <br>
+					Juillet 2015 - Aout 2015 :<strong> Stage assistant ingenieur bureau d'etude d'execution,</strong> ATPenergy, France <br>
+					 <li>Étude et dimensionnement d'un system de ventilation mécanique</li>
+					 <li>Réalisation des plans de masse sous AutoCAD</li>
+					 <li>Participation à l'étude de la renovation du systeme de sécurite incendie du centre pénitentiare de Perpignan</li>
+					</li>
+			</div>		
+		</div>
+	<div class="centre">
+	
+		<!-- <span style ="text-decoration: underline;">	 -->
+		<h2>CENTES D'INTERET
+		<button style="font-size: 50%;height:20px;padding-left:20px;padding-right:20px;padding-bottom:20px;background:#e8491d;border :#cccccc 2px solid" onclick="afficher('centre');">cacher</button>
+		</h2>
+		<!-- </span> -->
+	</div>	
+	<div id="centre">
+	<div class="centree">
+					<strong>Lecteur, Sportif, Voyage, Series</strong> <br>
+					<strong>Enseignant réferant :</strong> Monsieur Jean Patrick GELAS: <email>jean-patrick.gelas@univ-lyon1.fr</email> <br>
+					<a href="http://master-info.univ-lyon1.fr/CCI/#2" target=_blank>Lien de la formation</a> <br>
+	</div>	
+	
+	
+		</div>
+	</body>
+</html>
+		<section id="newsletter">
+		
+			<div class="contenu">
+				<h1>souscrivez à mes newsletters </h1>
+					<form>
+						<input type="email" placeholder="entrez votre email">
+						<button typpe="submit" class ="button_1"> envoyer</button>
+					</form>
+		
+		</section>
+		
+		
+		
+	
+		
+		
+		
+		
+		<footer>
+			<p>Magueye NDIAYE web design Copyright &copy;2017</p>
+			<section class="reseau">
+			<p>Cliquez ici pour voir mon compte linkedin<a href="https://fr.linkedin.com/in/magueye-ndiaye-99334b108" target=_blank><img src="..\_photos\linkedin.png" > </a></p>
+			</section>
+		</footer>
+		
+	</body>
+
+
+</html>
